1cf7080675ecbe24667997f5773008ccda26c7e4
tko-cactus <37374201+tko-cactus@users.noreply.github.com>
2021-01-03 23:57:13 +0900

    fixed TaxRate.py

--- smbc2019/B_TaxRate.py
+++ smbc2019/B_TaxRate.py
@@ -2,21 +2,13 @@ import math
 
 
 def main(input):
-    tmp = rounding(input / 1.08)
-    if input == (tmp * 1.08 // 1):
-        print(tmp)
+    X = math.ceil(input / 1.08)
+    if math.floor(X * 1.08) == input:
+        print(X)
     else:
-        print(':(')
-
-
-def rounding(num):
-    a = num // 1
-    if num >= a + 0.5:
-        return int(a + 1)
-    else:
-        return int(a)
+        print(":(")
 
 
 if __name__ == '__main__':
-    input = input()
-    main(int(input))
+    input = int(input())
+    main(input)
